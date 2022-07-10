ecfdead2910ce9e806db266eea79836eb6dd07bd
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-10 15:22:46 +0700

    Initial commit 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd     new file:   pom.xml 	new file:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/CurrencyConvertionServiceApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/github/thecoolersuptelov/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java

---
+++ src/test/java/com/github/thecoolersuptelov/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.currencyconvertionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
