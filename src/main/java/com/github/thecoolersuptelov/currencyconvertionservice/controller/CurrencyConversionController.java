fa504c51c94bf70951f18ad7a874abefe0399904
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-10 21:07:15 +0700

    Add OpenFeign dependency 	modified:   pom.xml 	modified:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/controller/CurrencyConversionController.java

--- src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/controller/CurrencyConversionController.java
@@ -21,6 +21,7 @@ public class CurrencyConversionController {
     }
     @GetMapping(value = "from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable @NonNull String fromCurrency, @PathVariable @NonNull String toCurrency, @PathVariable @NonNull Long quantity) {
+
         var uriVariables = new HashMap<String, String>();
         uriVariables.put("fromCurrency", fromCurrency);
         uriVariables.put("toCurrency", toCurrency);
@@ -32,6 +33,7 @@ public class CurrencyConversionController {
                 ).getBody();
         xx.setTotalCalculatedAmount(quantity*xx.getConvertionMultiple());
         xx.setQuantity(quantity);
+
         return xx.setEnviroment(environment.getProperty("local.server.port"));
     //    return new CurrencyConversion(fromCurrency,toCurrency,quantity, 0l, 0l).setEnviroment(environment.getProperty("local.server.port"));
     }
