7c1686cc204a9d1b11f3b1f89eb4e277503df913
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-10 21:25:35 +0700

    Done openFeign integration. 	modified:   pom.xml 	modified:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/CurrencyConvertionServiceApplication.java 	modified:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/controller/CurrencyConversionController.java 	new file:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/proxy/CurrencyExchange.java

---
+++ src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/proxy/CurrencyExchange.java
@@ -0,0 +1,15 @@
+package com.github.thecoolersuptelov.currencyconvertionservice.proxy;
+
+import com.github.thecoolersuptelov.currencyconvertionservice.domain.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "http://localhost:8000/currency-exchange/")
+public interface CurrencyExchange {
+
+    @GetMapping(value = "from/{fromCurrency}/to/{toCurrency}")
+    public CurrencyConversion retriveExchangeValue(@PathVariable @NonNull String fromCurrency, @PathVariable @NonNull String toCurrency);
+
+}
