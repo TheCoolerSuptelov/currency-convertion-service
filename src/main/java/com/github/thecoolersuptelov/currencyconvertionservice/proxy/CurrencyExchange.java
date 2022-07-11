650a2136fdd9615fae2fa16f8800ffc1631ef380
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-11 19:06:36 +0700

    Change exchange server address from static url to dynamic service finding by name using Eureka 	modified:   src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/proxy/CurrencyExchange.java

--- src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/proxy/CurrencyExchange.java
+++ src/main/java/com/github/thecoolersuptelov/currencyconvertionservice/proxy/CurrencyExchange.java
@@ -6,7 +6,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "http://localhost:8000/currency-exchange/")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchange {
 
     @GetMapping(value = "from/{fromCurrency}/to/{toCurrency}")
